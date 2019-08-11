93f5fd50cf365c6df77ef96687794c1c635eff62
New <chauhanraunaq878@gmail.com>
2019-08-11 13:31:27 +0530

    question object improved

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const startButton = document.getElementById('start-button');
 let questions;
+let questionNumber = 1;
 
 //start-quiz
 startButton.addEventListener('click', startQuiz);
@@ -18,6 +19,13 @@ function fetchQuestions() {
 
 function transferQuestions(questionSet) {
     questions = questionSet;
+    for(let i = 0; i < questions.length; i++) {
+      questions[i].questionNumber = i + 1;
+      questions[i].options = [];
+      questions[i].options.push(...questions[i].incorrect_answers);
+      let index = Math.round(Math.random() * 3);
+      questions[i].options.splice(index, 0, questions[i].correct_answer);
+    }
     console.log(questions);
 }
 
