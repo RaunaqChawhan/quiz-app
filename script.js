b874710aa3c21a47af8dad6d59a660460ff2443d
New <chauhanraunaq878@gmail.com>
2019-08-11 17:48:17 +0530

    Adding styling

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+const header = document.querySelector('.intro');
 const startButton = document.getElementById('start-button');
 const questionOptions = document.querySelector('.questions-options');
+const answer = document.querySelector('.answer');
 let questions;
 let questionNumber = 1;
 
 //start-quiz
 startButton.addEventListener('click', startQuiz);
-
+answer.addEventListener('dragover', dragOver);
+answer.addEventListener('dragenter', dragEnter);
+answer.addEventListener('dragleave', dragLeave);
+answer.addEventListener('drop', dragDrop);
 
 //Quiz functions
 function fetchQuestions() {
@@ -39,16 +44,43 @@ function displayQuestion() {
       const options = questions[i].options;
       questionOptions.innerHTML = `
         <h1 class="question">${question}</h1>
-        <p class="options" draggable="true" ondragstart="dragStart()">${options[i]}</p>
-        <p class="options" draggable="true" ondragstart="dragStart()">${options[i + 1]}</p>
-        <p class="options" draggable="true" ondragstart="dragStart()">${options[i + 2]}</p>
-        <p class="options" draggable="true" ondragstart="dragStart()">${options[i + 3]}</p>
-      `;
+        <p class="options" draggable="true" ondragstart="dragStart()" ondragend="dragEnd()" data-option="${options[i]}">${options[i]}</p>
+        <p class="options" draggable="true" ondragstart="dragStart()" ondragend="dragEnd()" data-option="${options[i + 1]}">${options[i + 1]}</p>
+        <p class="options" draggable="true" ondragstart="dragStart()" ondragend="dragEnd()" data-option="${options[i + 2]}">${options[i + 2]}</p>
+        <p class="options" draggable="true" ondragstart="dragStart()" ondragend="dragEnd()" data-option="${options[i + 3]}">${options[i + 3]}</p>
+        `;
       // console.log('working');
     }
   }
 }
 
+//Drag functions
+function dragStart() {
+
+}
+
+function dragEnd() {
+
+}
+
+function dragOver() {
+
+}
+
+function dragEnter() {
+
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+
+}
+
 function startQuiz() {
     fetchQuestions();
+    setTimeout(() => header.classList.add('hide'), 300);
+    // header.classList.add('hide');
 }
